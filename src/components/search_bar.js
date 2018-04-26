baafdddcd043717c1f56a2a1197d6b8bcb9f6c94
addison.pan <addisonp@gmail.com>
2018-04-25 22:53:28 -0700

    change component to class with default values via a valid search to youtube

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -2,9 +2,30 @@ import React, { Component } from 'react';
 
 // React.Component converted to just Component since we are importing the React.Component as Component
 class SearchBar extends Component {
-    render() {
-        return <input  />;
+    constructor(props) {
+        super(props); // initialize state in class base component
+
+        this.state = { term: '' };
     }
+    render() {
+        // return <input onChange={this.onInputChange} />;
+        // combine the function called by the onChange
+        // return <input onChange={(event) => console.log(event.target.value)} />;
+        // single line arrow function, a single argument can drop off leading parenthesis
+        // return <input onChange={event => console.log(event.target.value)} />;
+        // to update the state
+        return (
+            <div>
+                <input
+                    value={this.state.term}
+                    onChange={event => this.setState({ term: event.target.value })} />
+            </div>
+        );
+    }
+
+    // onInputChange(event) {
+    //     console.log(event.target.value);
+    // }
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-// relative path from this file
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDpQ-J7bT3jy3aQ0AmaVrzf01CzvNGUfcY';
 
-// Create a new component.  This component should contain some HTML
-const App = () => {
-    return (
-        <div>
-        <SearchBar />
-    </div>
-    );
+
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {videos: []};
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            this.setState({ videos }); // equivalent to { videos : videos } where key and prop same name
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar/>
+            </div>
+        );
+    }
 }
 
+
 // Take this component's generated HTML and put it on the page (in the DOM)
-ReactDOM.render(<App />, document.querySelector(".container"));
+ReactDOM.render(<App/>, document.querySelector(".container"));
